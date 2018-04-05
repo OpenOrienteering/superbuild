03530dac7886441e0f7b38bb91dbf24798cd078d
Kai Pastor <dg0yt@darc.de>
2018-04-05 07:33:26 +0200

    pcre3: Fix description of USE_SYSTEM_PCRE3

--- pcre3-8.39-3.cmake
+++ pcre3-8.39-3.cmake
@@ -32,7 +32,7 @@ set(download_hash  SHA256=b858099f82483031ee02092711689e7245586ada49e534a06e678b
 set(patch_version  ${version}-3)
 set(patch_hash     SHA256=a9f0e1a96b6a017965fe69233e267682c289f2cfeb33b46fb78aedcb8cf2c16a)
 
-option(USE_SYSTEM_PCRE3 "Use the system sqlite if possible" ON)
+option(USE_SYSTEM_PCRE3 "Use the system pcre3 if possible" ON)
 
 set(test_system_pcre3 [[
 	if(USE_SYSTEM_PCRE3)
