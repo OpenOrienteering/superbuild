cca9f6a7cc01d5aa4ced7529dcf8ce773be3a09e
Kai Pastor <dg0yt@darc.de>
2018-12-12 09:38:14 +0100

    Add Qt 5.12.0

---
+++ qt-5.12.0-0.cmake
@@ -0,0 +1,453 @@
+# This file is part of OpenOrienteering.
+
+# Copyright 2016-2018 Kai Pastor
+#
+# Redistribution and use is allowed according to the terms of the BSD license:
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 
+# 1. Redistributions of source code must retain the copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote products 
+#    derived from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# No Debian package yet.
+set(short_version  5.12)
+set(version        5.12.0)
+set(patch_version  ${version}-0)
+
+option(USE_SYSTEM_QT "Use the system Qt if possible" ON)
+
+string(CONFIGURE [[
+	if("${module}" MATCHES "Android" AND NOT ANDROID)
+		set(BUILD_CONDITION 0)
+	elseif(ANDROID AND "${module}" STREQUAL "Qt5SerialPort")
+		set(BUILD_CONDITION 0)
+	elseif(USE_SYSTEM_QT)
+		find_package(Qt5Core @version@ CONFIG QUIET
+		  NO_CMAKE_FIND_ROOT_PATH
+		  NO_CMAKE_SYSTEM_PATH
+		  NO_SYSTEM_ENVIRONMENT_PATH
+		)
+		if(Qt5Core_VERSION)
+			find_package(${module} ${Qt5Core_VERSION} CONFIG EXACT
+			  NO_CMAKE_FIND_ROOT_PATH
+			  NO_CMAKE_SYSTEM_PATH
+			  NO_SYSTEM_ENVIRONMENT_PATH
+			)
+			find_package(${module} ${Qt5Core_VERSION_MAJOR}.${Qt5Core_VERSION_MINOR} CONFIG REQUIRED
+			  NO_CMAKE_FIND_ROOT_PATH
+			  NO_CMAKE_SYSTEM_PATH
+			  NO_SYSTEM_ENVIRONMENT_PATH
+			)
+			if(${module}_VERSION
+			   AND (NOT ${module}_INCLUDE_DIRS
+			        OR NOT "${module}_INCLUDE_DIRS" MATCHES "${INSTALL_DIR}${CMAKE_INSTALL_PREFIX}"))
+				message(STATUS "Found ${SYSTEM_NAME} ${module}: ${${module}_VERSION}")
+				set(BUILD_CONDITION 0)
+			else()
+				message(STATUS "Found ${SYSTEM_NAME} Qt5Core ${Qt5Core_VERSION}, but no matching ${module}")
+			endif()
+		endif()
+	endif()
+]] use_system_qt @ONLY)
+
+
+
+# extras for superbuild support
+
+set(default        [[$<STREQUAL:${SYSTEM_NAME},default>]])
+set(crosscompiling [[$<BOOL:${CMAKE_CROSSCOMPILING}>]])
+set(windows        [[$<STREQUAL:${CMAKE_SYSTEM_NAME},Windows>]])
+set(macos          [[$<STREQUAL:${CMAKE_SYSTEM_NAME},Darwin>]])
+set(android        [[$<BOOL:${ANDROID}>]])
+set(use_sysroot    [[$<NOT:$<AND:$<BOOL:${CMAKE_CROSSCOMPILING}>,$<BOOL:${ANDROID}>>>]])
+set(qmake          [[$<$<BOOL:${CMAKE_CROSSCOMPILING}>:${TOOLCHAIN_DIR}>$<$<NOT:$<BOOL:${CMAKE_CROSSCOMPILING}>>:${INSTALL_DIR}${CMAKE_INSTALL_PREFIX}>/bin/qmake]])
+
+
+set(module Qt5Core)
+superbuild_package(
+  NAME           qt-superbuild
+  VERSION        ${patch_version}
+  DEPENDS
+    common-licenses
+  
+  SOURCE
+    URL            https://github.com/OpenOrienteering/superbuild/archive/qt-superbuild_${patch_version}.tar.gz
+    URL_HASH       SHA256=cc9f62b010ed3a96c432e0c248c1e0495c53c4027b93763109cdf7752cd9e614
+  
+  BUILD [[
+    CMAKE_ARGS
+      "-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}"
+    BUILD_COMMAND ""
+    INSTALL_COMMAND
+      "${CMAKE_COMMAND}" -E env "DESTDIR=${INSTALL_DIR}"
+      "${CMAKE_COMMAND}" -P cmake_install.cmake
+  ]]
+)
+
+
+
+# qtbase
+
+superbuild_package(
+  NAME           qtbase
+  VERSION        ${short_version}
+  DEPENDS
+    qtbase-opensource-src-${version}
+)
+
+set(module Qt5Core)
+superbuild_package(
+  NAME         qtbase-opensource-src
+  VERSION      ${version}
+  DEPENDS
+    libjpeg-turbo
+    libpng
+    pcre2
+    sqlite3
+    zlib
+  
+  SOURCE
+    URL             https://download.qt.io/archive/qt/${short_version}/${version}/submodules/qtbase-everywhere-src-${version}.tar.xz
+    URL_HASH        SHA256=5e03221d780e121aabd734896aab8f331e5d8c9d9b54f1eb04907d0818eaeecb
+    
+    # Don't accidently used bundled copies
+    PATCH_COMMAND
+      "${CMAKE_COMMAND}" -E copy_directory src/3rdparty/libjpeg src/3rdparty/libjpeg.unused
+    COMMAND
+      "${CMAKE_COMMAND}" -E remove_directory src/3rdparty/libjpeg
+    COMMAND
+      "${CMAKE_COMMAND}" -E copy_directory src/3rdparty/libpng src/3rdparty/libpng.unused
+    COMMAND
+      "${CMAKE_COMMAND}" -E remove_directory src/3rdparty/libpng
+    COMMAND
+      "${CMAKE_COMMAND}" -E copy_directory src/3rdparty/pcre src/3rdparty/pcre.unused
+    COMMAND
+      "${CMAKE_COMMAND}" -E remove_directory src/3rdparty/pcre
+    COMMAND
+      "${CMAKE_COMMAND}" -E copy_directory src/3rdparty/sqlite src/3rdparty/sqlite.unused
+    COMMAND
+      "${CMAKE_COMMAND}" -E remove_directory src/3rdparty/sqlite
+    COMMAND
+      "${CMAKE_COMMAND}" -E copy_directory src/3rdparty/zlib src/3rdparty/zlib.unused
+    COMMAND
+      "${CMAKE_COMMAND}" -E remove -f src/3rdparty/zlib/*.c
+  
+  USING default crosscompiling windows android macos USE_SYSTEM_QT module
+  BUILD_CONDITION  ${use_system_qt}
+  BUILD [[
+    CONFIGURE_COMMAND 
+    $<${crosscompiling}:
+      COMMAND
+       # Cf. qtbase configure "SYSTEM_VARIABLES"
+       "${CMAKE_COMMAND}" -E env 
+         --unset=AR
+         --unset=RANLIB
+         --unset=STRIP
+         --unset=OBJDUMP
+         --unset=LD
+         --unset=CC
+         --unset=CXX
+         --unset=CFLAGS
+         --unset=CXXFLAGS
+         --unset=LDFLAGS
+         # fall through
+    >
+    "${SOURCE_DIR}/configure"
+      -opensource
+      -confirm-license
+      $<$<CONFIG:Debug>:-debug>$<$<NOT:$<CONFIG:Debug>>:-release $<$<CONFIG:RelWithDebInfo>:-force-debug-info>>
+      -shared
+      $<${macos}:-no-framework>
+      -gui
+      -widgets
+      -system-libjpeg
+      -system-libpng
+      -system-pcre
+      -system-zlib
+      -no-sql-db2
+      -no-sql-ibase
+      -no-sql-mysql
+      -no-sql-oci
+      -no-sql-odbc
+      -no-sql-psql
+      -no-sql-sqlite
+      -no-sql-sqlite2
+      -no-sql-tds
+      -no-openssl
+      -no-directfb
+      -no-linuxfb
+      $<$<OR:${android},${macos},${windows}>:
+        -no-dbus
+      >
+      -make tools
+      -nomake examples
+      -nomake tests
+      -system-proxies
+      -no-glib
+      -prefix "${CMAKE_INSTALL_PREFIX}"
+      -datadir "${CMAKE_INSTALL_PREFIX}/share"
+      -extprefix "${INSTALL_DIR}${CMAKE_INSTALL_PREFIX}"
+      $<${crosscompiling}:
+        -no-pkg-config
+        -hostprefix "${TOOLCHAIN_DIR}"
+        $<${windows}:
+          -device-option CROSS_COMPILE=${SUPERBUILD_TOOLCHAIN_TRIPLET}-
+          -xplatform     win32-g++
+          -opengl desktop
+        >
+        $<${android}:
+          $<$<STREQUAL:${CMAKE_CXX_COMPILER_ID},GNU>:
+            -xplatform     android-g++
+          >$<$<STREQUAL:${CMAKE_CXX_COMPILER_ID},Clang>:
+            -xplatform     android-clang
+            -disable-rpath
+          >
+          -android-ndk   "${ANDROID_NDK_ROOT}"
+          -android-sdk   "${ANDROID_SDK_ROOT}"
+          -android-arch  "${ANDROID_ABI}"
+          -android-ndk-platform "${ANDROID_PLATFORM}"
+        >
+      >
+      -I "${INSTALL_DIR}${CMAKE_INSTALL_PREFIX}/include"
+      -L "${INSTALL_DIR}${CMAKE_INSTALL_PREFIX}/lib"
+  ]]
+)
+
+
+
+# qtandroidextras
+
+superbuild_package(
+  NAME           qtandroidextras
+  VERSION        ${short_version}
+  DEPENDS        qtandroidextras-opensource-src-${version}
+)
+
+set(module Qt5AndroidExtras)
+superbuild_package(
+  NAME           qtandroidextras-opensource-src
+  VERSION        ${version}
+  DEPENDS        qtbase-${short_version}
+  
+  SOURCE
+    URL             https://download.qt.io/archive/qt/${short_version}/${version}/submodules/qtandroidextras-everywhere-src-${version}.tar.xz
+    URL_HASH        SHA256=f5a9a13bf8b8999250b27fd68c1ed4287f333d1cebed05ae9d0b29bf9f1d0868
+  
+  USING qmake USE_SYSTEM_QT module
+  BUILD_CONDITION  ${use_system_qt}
+  BUILD [[
+    CONFIGURE_COMMAND
+      "${qmake}" "${SOURCE_DIR}"
+  ]]
+)
+
+
+
+# qtimageformats
+
+superbuild_package(
+  NAME           qtimageformats
+  VERSION        ${short_version}
+  DEPENDS        qtimageformats-opensource-src-${version}
+)
+
+set(module Qt5Gui) # qtimageformats adds plugins to Qt5Gui
+superbuild_package(
+  NAME           qtimageformats-opensource-src
+  VERSION        ${version}
+  DEPENDS        qtbase-${short_version}
+                 tiff
+  
+  SOURCE
+    URL             https://download.qt.io/archive/qt/${short_version}/${version}/submodules/qtimageformats-everywhere-src-${version}.tar.xz
+    URL_HASH        SHA256=5f907de96d3db6276816f321b7e11cba7beb1fcc81bcb3f1729cde3b7599732e
+    
+    # Don't accidently used bundled copies
+    PATCH_COMMAND
+      "${CMAKE_COMMAND}" -E copy_directory src/3rdparty/jasper src/3rdparty/jasper.unused
+    COMMAND
+      "${CMAKE_COMMAND}" -E remove_directory src/3rdparty/jasper
+    COMMAND
+      "${CMAKE_COMMAND}" -E copy_directory src/3rdparty/libtiff src/3rdparty/libtiff.unused
+    COMMAND
+      "${CMAKE_COMMAND}" -E remove_directory src/3rdparty/libtiff
+  
+  USING qmake USE_SYSTEM_QT module
+  BUILD_CONDITION  ${use_system_qt}
+  BUILD [[
+    CONFIGURE_COMMAND
+      "${qmake}" "${SOURCE_DIR}"
+  ]]
+)
+
+
+
+# qtlocation
+
+superbuild_package(
+  NAME           qtlocation
+  VERSION        ${short_version}
+  DEPENDS        qtlocation-opensource-src-${version}
+)
+
+set(module Qt5Location)
+superbuild_package(
+  NAME           qtlocation-opensource-src
+  VERSION        ${version}
+  DEPENDS        qtbase-${short_version}
+                 qtserialport-${short_version}
+  
+  SOURCE
+    URL             https://download.qt.io/archive/qt/${short_version}/${version}/submodules/qtlocation-everywhere-src-${version}.tar.xz
+    URL_HASH        SHA256=b0a3134df3a00f6045405d3c12e0ea3ae7e640feaf38e238873df85424674449
+    
+  USING qmake USE_SYSTEM_QT module
+  BUILD_CONDITION  ${use_system_qt}
+  BUILD [[
+    CONFIGURE_COMMAND
+      "${qmake}" "${SOURCE_DIR}"
+  ]]
+)
+
+
+
+# qtsensors
+
+superbuild_package(
+  NAME           qtsensors
+  VERSION        ${short_version}
+  DEPENDS        qtsensors-opensource-src-${version}
+)
+
+set(module Qt5Sensors)
+superbuild_package(
+  NAME           qtsensors-opensource-src
+  VERSION        ${version}
+  DEPENDS        qtbase-${short_version}
+  
+  SOURCE
+    URL             https://download.qt.io/archive/qt/${short_version}/${version}/submodules/qtsensors-everywhere-src-${version}.tar.xz
+    URL_HASH        SHA256=c53132847a1f70d5a4c5fe689a9943d8afae3f3648bf5b0d824015093addc5a6
+  
+  USING qmake USE_SYSTEM_QT module
+  BUILD_CONDITION  ${use_system_qt}
+  BUILD [[
+    CONFIGURE_COMMAND
+      "${qmake}" "${SOURCE_DIR}"
+  ]]
+)
+
+
+
+# qtserialport
+
+superbuild_package(
+  NAME           qtserialport
+  VERSION        ${short_version}
+  DEPENDS        qtserialport-opensource-src-${version}
+)
+
+set(module Qt5SerialPort)
+superbuild_package(
+  NAME           qtserialport-opensource-src
+  VERSION        ${version}
+  DEPENDS        qtbase-${short_version}
+  
+  SOURCE
+    URL             https://download.qt.io/archive/qt/${short_version}/${version}/submodules/qtserialport-everywhere-src-${version}.tar.xz
+    URL_HASH        SHA256=59d4005695089c6b50fc5123094125d9224c1715655128640158a2404075a9bb
+  
+  USING qmake USE_SYSTEM_QT module
+  BUILD_CONDITION  ${use_system_qt}
+  BUILD [[
+    CONFIGURE_COMMAND
+      "${qmake}" "${SOURCE_DIR}"
+  ]]
+)
+
+
+
+# qttools
+
+superbuild_package(
+  NAME           qttools
+  VERSION        ${short_version}
+  DEPENDS        qttools-opensource-src-${version}
+)
+
+set(qttools_install_android
+  INSTALL_COMMAND
+    "$(MAKE)" -C src/linguist/lconvert install
+  COMMAND
+    "$(MAKE)" -C src/linguist/lrelease install
+  COMMAND
+    "$(MAKE)" -C src/linguist/lupdate install
+  COMMAND
+    "$(MAKE)" -C src/linguist install_cmake_linguist_tools_files
+)
+
+set(module Qt5LinguistTools)
+superbuild_package(
+  NAME           qttools-opensource-src
+  VERSION        ${version}
+  DEPENDS        qtbase-${short_version}
+  
+  SOURCE
+    URL             https://download.qt.io/archive/qt/${short_version}/${version}/submodules/qttools-everywhere-src-${version}.tar.xz
+    URL_HASH        SHA256=574ce34b6e5bcd5dce4020a3947730f3c2223eee65d0396a311099223364dac3
+  
+  USING qmake qttools_install_android USE_SYSTEM_QT module
+  BUILD_CONDITION  ${use_system_qt}
+  BUILD [[
+    CONFIGURE_COMMAND
+      "${qmake}" "${SOURCE_DIR}"
+    $<$<AND:$<BOOL:${CMAKE_CROSSCOMPILING}>,$<BOOL:${ANDROID}>>:${qttools_install_android}>
+  ]]
+)
+
+
+
+# qttranslations
+
+superbuild_package(
+  NAME           qttranslations
+  VERSION        ${short_version}
+  DEPENDS        qttranslations-opensource-src-${version}
+)
+
+set(module Qt5Core) # Can't find qttranslations via CMake.
+superbuild_package(
+  NAME           qttranslations-opensource-src
+  VERSION        ${version}
+  DEPENDS        qtbase-${short_version}
+                 qttools-${short_version}
+  
+  SOURCE
+    URL             https://download.qt.io/archive/qt/${short_version}/${version}/submodules/qttranslations-everywhere-src-${version}.tar.xz
+    URL_HASH        SHA256=5b4f186e0b96703041319b5b131393b6aa829ea74e067697ede548d936327508
+  
+  USING qmake USE_SYSTEM_QT module
+  BUILD_CONDITION  ${use_system_qt}
+  BUILD [[
+    CONFIGURE_COMMAND
+      "${qmake}" "${SOURCE_DIR}"
+  ]]
+)
